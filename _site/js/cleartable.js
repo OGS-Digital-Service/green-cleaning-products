4f4cf065adbd48368f1bee29d84f7065a3b0422c
Jeff Knaack <58082485+knaackbuilt@users.noreply.github.com>
2024-03-12 12:33:12 -0400

    re add resources

---
+++ _site/js/cleartable.js
@@ -0,0 +1,13 @@
+function ClearFilters() {
+  $('.clearthis').val('');  
+  $('.dataTables_filter').val('');
+	var table = $('#productstable').DataTable();
+	table
+	.search( '' )
+	.columns().search( '' )
+	.draw();}
+
+
+
+
+
